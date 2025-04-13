d379e68ae05203bade2283e85eea3e601d25fe7e
pankajsingh <pankajrajput1462@gmail.com>
2025-04-13 17:13:29 +0530

    Added some comments

--- src/main/java/pan/leetcode/App.java
+++ src/main/java/pan/leetcode/App.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class App {
 
     public static void main(String... args) {
-        Logger logger=Logger.getLogger(App.class.getName());
-        logger.info("This is a module-using Hello World!");
+        Logger logger = Logger.getLogger(App.class.getName());
+        logger.info("This is a leetcode Learn Area");
     }
 }
